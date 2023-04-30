202d5036482d6930813448507fe24d343f91a670
bestag0x <matteocaligiana@gmail.com>
2023-04-30 15:32:02 +0200

    ff

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import * as THREE from 'three'
 import { useEffect, useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { useCursor, MeshReflectorMaterial, Image, Text, Environment } from '@react-three/drei'
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
+import { useCursor, MeshReflectorMaterial, Image, Text, Environment, useTexture, Html, Preload, Scroll, ScrollControls } from '@react-three/drei'
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+
 import { useRoute, useLocation } from 'wouter'
 import { easing } from 'maath'
 import getUuid from 'uuid-by-string'
 
-const GOLDENRATIO = 1.61803398875
 
-export const App = ({ images }) => (
+const GOLDENRATIO = 1.61803398875
+export const App = ({ images }) => {
+
+  return(
   <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
     <color attach="background" args={['#191920']} />
     <fog attach="fog" args={['#191920', 0, 15]} />
+    <ScrollControls damping={0.5} pages={1.7}>
+
+    <Scroll>
     <group position={[0, -0.5, 0]}>
       <Frames images={images} />
       <mesh rotation={[-Math.PI / 2, 0, 0]}>
@@ -27,24 +34,29 @@ export const App = ({ images }) => (
           maxDepthThreshold={1.4}
           color="#050505"
           metalness={0.5}
-        />
+          />
       </mesh>
     </group>
+    </Scroll>
+          </ScrollControls>
     <Environment preset="city" />
   </Canvas>
-)
+
+)}
 
 function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() }) {
   const ref = useRef()
   const clicked = useRef()
   const [, params] = useRoute('/item/:id')
   const [, setLocation] = useLocation()
+  
   useEffect(() => {
     clicked.current = ref.current.getObjectByName(params?.id)
     if (clicked.current) {
       clicked.current.parent.updateWorldMatrix(true, true)
       clicked.current.parent.localToWorld(p.set(0, GOLDENRATIO / 2, 1.25))
       clicked.current.parent.getWorldQuaternion(q)
+
     } else {
       p.set(0, 0, 5.5)
       q.identity()
@@ -57,46 +69,80 @@ function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() })
   return (
     <group
       ref={ref}
-      onClick={(e) => (e.stopPropagation(), setLocation(clicked.current === e.object ? '/' : '/item/' + e.object.name))}
+      //      onClick={(e) => (e.stopPropagation(), setLocation(clicked.current === e.object ? '/' : '/item/' + e.object.name))}
+      onClick={(e) => (e.stopPropagation(), setLocation('/item/' + e.object.name))}
+
       onPointerMissed={() => setLocation('/')}>
       {images.map((props) => <Frame key={props.url} {...props} /> /* prettier-ignore */)}
     </group>
   )
 }
 
-function Frame({ url, c = new THREE.Color(), ...props }) {
+function Frame({ url, text, desc, link, buyImage, c = new THREE.Color(), ...props }) {
   const image = useRef()
+  const image22 = useRef()
   const frame = useRef()
   const [, params] = useRoute('/item/:id')
   const [hovered, hover] = useState(false)
   const [rnd] = useState(() => Math.random())
   const name = getUuid(url)
   const isActive = params?.id === name
+  const [isVisible, setIsVisible] = useState(false);
+
+  const buyImgUrl = '/15_normal.png'
   useCursor(hovered)
   useFrame((state, dt) => {
-    image.current.material.zoom = 2 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2
-    easing.damp3(image.current.scale, [0.85 * (!isActive && hovered ? 0.85 : 1), 0.9 * (!isActive && hovered ? 0.905 : 1), 1], 0.1, dt)
+    image22.current.material.zoom = 1 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2
+    easing.damp3(image.current.scale, [0.85 * (!isActive && hovered ? 0.85 : 1.1), 0.9 * (!isActive && hovered ? 0.905 : 1.1), 1], 0.1, dt)
+    
     easing.dampC(frame.current.material.color, hovered ? 'orange' : 'white', 0.1, dt)
+    easing.damp3(image22.current.scale, [0.85 * (!isActive && hovered ? 0.85 : 1), 0.9 * (!isActive && hovered ? 0.905 : 1), 1], 0.1, dt)
+    image.current.material.opacity = (isActive && hovered ? 0.3 : 0)
+    image.current.material.colorWrite = (isActive && hovered ? 1 : 0)
+    isActive ? setIsVisible(true) : setIsVisible(false)
+
   })
+  const imageMapName = (type) => `/15_${type}.jpg`;
+  const colorMap = useLoader(TextureLoader, imageMapName('normal'))
+
   return (
     <group {...props}>
       <mesh
         name={name}
         onPointerOver={(e) => (e.stopPropagation(), hover(true))}
         onPointerOut={() => hover(false)}
+        onClick={(e) => (console.log(e))}
         scale={[1, GOLDENRATIO, 0.05]}
         position={[0, GOLDENRATIO / 2, 0]}>
         <boxGeometry />
-        <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
-        <mesh ref={frame} raycast={() => null} scale={[0.9, 0.93, 0.9]} position={[0, 0, 0.2]}>
+        <meshStandardMaterial map={colorMap} metalness={0.5}  
+          roughness={0.5}
+          envMapIntensity={1}
+          map-offset-x={0.52}
+          map-offset-y={0.3}
+          map-repeat-x={0.1}
+          map-repeat-y={0.5} />
+        <mesh  ref={frame} raycast={() => null} scale={[0.9, 0.93, 0.9]} position={[0, 0, 0.2]}>
           <boxGeometry />
           <meshBasicMaterial toneMapped={false} fog={false} />
         </mesh>
-        <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url} />
+        <Image  raycast={() => null} ref={image} position={[0, 0, 0.71]} transparent url={buyImgUrl} />
+        <Image raycast={() => null} ref={image22} position={[0, 0, 0.7]} url={url} />
+
       </mesh>
       <Text maxWidth={0.1} anchorX="left" anchorY="top" position={[0.55, GOLDENRATIO, 0]} fontSize={0.025}>
-        {name.split('-').join(' ')}
+        {text}
+      </Text>
+      <Text visible={isVisible} onClick={(e) => (e.stopPropagation(),window.open(
+            `${link}`, "_blank"))} maxWidth={0.1} anchorX="left" anchorY="top" position={[0.55, GOLDENRATIO-0.1, 0]} fontSize={0.035}>
+        Buy it on amazon
       </Text>
     </group>
   )
+
+  //      under box geometry
+  //      <meshStandardMaterial color="#155515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
+
+  //      inside <Text/>
+  //      {name.split('-').join(' ')}
 }
--- src/index.js
+++ src/index.js
@@ -2,21 +2,24 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import { App } from './App'
 
+const image = (id) => `/lib${id}.jpg`
 const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
 const images = [
   // Front
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
+  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: image(1), text:'Libro 1', desc:'1', link:''},
   // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
+  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: image(2), text:'Titolo 2', desc:'2' , link:'https://www.amazon.it/Cromonomi-%D7%94%D7%9B%D7%A8%D7%95%D7%9E%D7%95%D7%A0%D7%95%D7%9E%D7%99%D7%9D-Elisheva-Zinellu/dp/B0BGNMQY5L'},
+  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: image(3), text:'Libro 3', desc:'3', link:'' },
   // Left
-  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482) },
-  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185) },
-  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574) },
+  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: image(4), text:`Titolo libro 4
+  Descrizione:
+  `, desc:'4', link:'' },
+  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: image(5), text:'Libro 5' , desc:'5', link:''},
+  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: image(6), text:'Libro 6' , desc:'6', link:''},
   // Right
-  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675) },
-  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738) },
-  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986) }
+  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: image(7), text:'Libro 7' , desc:'', link:''},
+  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738), text:'Libro 8', desc:'' , link:''},
+  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986), text:'Libro 9', desc:'', link:'' }
 ]
 
 createRoot(document.getElementById('root')).render(<App images={images} />)
